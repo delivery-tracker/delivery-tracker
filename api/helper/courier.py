67d9d75e2c898c395a532af09e4d3c0459ae54d3
Fil Luzadas <flg.l@outlook.com>
2023-08-21 21:53:10 -0400

    Added conditional check for FedEx to check date

--- api/helper/courier.py
+++ api/helper/courier.py
@@ -8,8 +8,14 @@ def fedex(tracking):
     time.sleep(10)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    for delivery_date in soup.find_all("span", {"class": "deliveryDateText"}, True):
-        return (delivery_date.text)
+    # Need to check on whether a delivery date is available from courier
+    for delivery_date in soup.find_all("span", {"class": "deliveryDateTextBetween"}, True):
+        if (delivery_date.text != ""):
+            return (delivery_date.text)
+        else:
+            # If there isn't, check the status of what the page gives
+            for delivery_status in soup.find_all("span", {"class": "deliveryDateText"}, True):
+                return (delivery_status.text)
 
 def canada_post(tracking):
     driver = uc.Chrome(headless=True)
@@ -19,3 +25,6 @@ def canada_post(tracking):
 
     for delivery_date in soup.find_all("span", {"class": "text-typography-redesigned-medium edd-text-size ng-star-inserted"}, True):
         return (delivery_date.text)
+
+def purolator(tracking):
+    pass
