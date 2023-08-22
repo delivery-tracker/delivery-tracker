4854980271243d7779cfe03fa80522605451908e
Fil Luzadas <flg.l@outlook.com>
2023-08-21 21:28:26 -0400

    Added Canada Post functionality

--- api/helper/courier.py
+++ api/helper/courier.py
@@ -9,4 +9,13 @@ def fedex(tracking):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     for delivery_date in soup.find_all("span", {"class": "deliveryDateText"}, True):
-        return(delivery_date.text)
+        return (delivery_date.text)
+
+def canada_post(tracking):
+    driver = uc.Chrome(headless=True)
+    driver.get(f"https://www.canadapost-postescanada.ca/track-reperage/en#/details/{tracking}")
+    time.sleep(10)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    for delivery_date in soup.find_all("span", {"class": "text-typography-redesigned-medium edd-text-size ng-star-inserted"}, True):
+        return (delivery_date.text)
